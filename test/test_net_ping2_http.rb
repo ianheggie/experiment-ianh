cc0ad9108e4039031358e162d2a333f8b92da086
Ian Heggie <ian@heggie.biz>
2014-04-12 20:37:43 +1000

    cleanout more code

--- test/test_net_ping2_http.rb
+++ test/test_net_ping2_http.rb
@@ -4,14 +4,23 @@
 # Test case for the Net::PingHTTP class. This should be run via the 'test' or
 # 'test:http' Rake task.
 #################################################################################
-require File.expand_path('../test_helper.rb', __FILE__)
+
+require 'test-unit'
+
+LOCALHOST = 'localhost'
+BOGUS_HOST = 'foo.bar.baz'
+
+LOCALHOST_IP = '127.0.0.1'
+DEFAULT_BLACKHOLE_IP = '144.140.108.23' # telstra.com - aussie ISP that drops packets
+DEFAULT_BLACKHOLE_PORT = 1001
+
 require 'net/ping2/http'
 
 class TestNetPing2HTTP < Test::Unit::TestCase
   #extend TestHelper
 
   def setup
-    extend TestHelper
+    #extend TestHelper
     ENV['http_proxy'] = ENV['https_proxy'] = ENV['no_proxy'] = nil
 
     @ping = Net::Ping2::HTTP.new()
