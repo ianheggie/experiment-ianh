fff03929ca1539a4a290db466a069baa50e8ac9e
Ian Heggie <ian@heggie.biz>
2014-04-12 20:34:24 +1000

    remove unnessisary code

--- lib/net/ping2/http.rb
+++ lib/net/ping2/http.rb
@@ -25,39 +25,15 @@ module Net
       # of the final URI.  If this value is set to false, then it will not
       # follow a redirect and will return false immediately on a redirect.
       #
-      attr_accessor :follow_redirect
-
-      # The maximum number of redirects allowed. The default is 5.
-      attr_accessor :redirect_limit
-
-      # The user agent used for the HTTP request. The default is 'net-ping2'.
-      attr_accessor :user_agent
-
-      # OpenSSL certificate verification mode. The default is VERIFY_NONE.
-      attr_accessor :ssl_verify_mode
-
-      # Use GET request instead HEAD. The default is false.
-      attr_accessor :get_request
-
-      # was this ping proxied?
-      attr_reader :proxied
-
-      # For unsuccessful requests that return a server error, it is
-      # useful to know the HTTP status code of the response.
-      attr_reader :code
 
       # Creates and returns a new Net::Ping2::HTTP object.
       # The default port is 80,
       # The default timeout is 10 seconds.
       #
       def initialize(options = {})
-        @follow_redirect = true
-        @redirect_limit = 5
         @ssl_verify_mode = OpenSSL::SSL::VERIFY_NONE
-        @get_request = false
         @port = 80
         @port = URI.parse(options[:host]).port if options[:host]
-        #@code = nil
         @user_agent = 'net-ping2'
         super(options)
       end
@@ -83,9 +59,7 @@ module Net
 
         # See https://bugs.ruby-lang.org/issues/8645
         url = host || @host
-        if url =~ %r{^//}
-          url = "http:#{url}"
-        elsif url !~ %r{^https?://}
+        if url !~ %r{^https?://}
           url = "url://#{url}"
         end
         uri = URI.parse(url)
@@ -97,52 +71,9 @@ module Net
 
         timeout = options[:timeout] || @timeout
 
-        start_time = Time.now
+        do_ping(uri, port, timeout)
 
-        http_response = do_ping(uri, port, timeout)
-
-        if http_response.is_a?(Net::HTTPSuccess)
-          set_response(http_response)
-          @success = true
-        elsif redirect?(http_response) # Check code, HTTPRedirection does not always work
-          if @follow_redirect
-            @warning = http_response.message
-            rlimit = 0
-
-            while redirect?(http_response)
-              if rlimit >= redirect_limit
-                @exception = "Redirect limit exceeded"
-                break
-              end
-              redirect = URI.parse(http_response['location'])
-              redirect = uri + redirect if redirect.relative?
-              http_response = do_ping(redirect, port, timeout)
-              rlimit += 1
-            end
-
-            if http_response.is_a?(Net::HTTPSuccess)
-              set_response(http_response)
-              @success = true
-            else
-              @warning = nil
-              @exception ||= http_response.message
-            end
-
-          else
-            @exception = http_response.message
-          end
-        else
-          @exception ||= http_response.message
-        end
-
-        # There is no duration if the ping failed
-        @duration = Time.now - start_time if @success
-
-        @success
       end
-
-      alias follow_redirect? follow_redirect
-      alias proxied? proxied
 
       protected
 
@@ -153,31 +84,18 @@ module Net
 
       private
 
-      def redirect?(http_response)
-        http_response && http_response.code.to_i >= 300 && http_response.code.to_i < 400
-      end
-
       def do_ping(uri, port, timeout)
         http_response = nil
         proxy = uri.find_proxy || URI.parse("")
         uri_path = uri.path.empty? ? '/' : uri.path
         headers = {}
-        headers["User-Agent"] = user_agent if user_agent
+        headers["User-Agent"] = 'net-ping2'
         begin
           http = Net::HTTP::Proxy(proxy.host, proxy.port, proxy.user, proxy.password).new(uri.host, port)
           http.read_timeout = timeout
           http.open_timeout = timeout
           @proxied = http.proxy?
-          if @get_request == true
-            request = Net::HTTP::Get.new(uri_path)
-          else
-            request = Net::HTTP::Head.new(uri_path)
-          end
-
-          if uri.scheme == 'https'
-            http.use_ssl = true
-            http.verify_mode = @ssl_verify_mode
-          end
+          request = Net::HTTP::Head.new(uri_path)
           http_response = Timeout.timeout(timeout) do
             http.start { |h| h.request(request) }
           end
@@ -186,22 +104,7 @@ module Net
         ensure
           http.open_timeout = 999  # DEBUG
         end
-        @code = http_response.code if http_response
-        http_response
       end
-
-
-      def set_response(http_response)
-        if @get_request
-          @response = http_response.body
-        else
-          @response = "HTTP#{http_response.http_version ? ('/' << http_response.http_version) : ''} #{http_response.code} #{http_response.message}\n"
-          http_response.header.each do |key, value|
-            @response << "#{key}: #{value}\n"
-          end
-        end
-      end
-
     end
   end
 end
