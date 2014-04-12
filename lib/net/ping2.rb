9d4e86798ed9021b9e35cf7965291f5853d1044b
Ian Heggie <ian@heggie.biz>
2014-04-12 19:28:52 +1000

    removed unessisary files

--- lib/net/ping2.rb
+++ lib/net/ping2.rb
@@ -4,12 +4,7 @@ require "net/ping2/version"
 # want to require a specific ping type only, do "require 'net/ping2/tcp'",
 # for example.
 
-require 'net/ping2/tcp'
-require 'net/ping2/udp'
-require 'net/ping2/icmp'
-require 'net/ping2/external'
 require 'net/ping2/http'
-require 'net/ping2/wmi'
 
 module Net
   module Ping2
